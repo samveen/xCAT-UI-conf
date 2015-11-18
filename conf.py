ffb998d1a94fdfd87976f7ef7dae2fb8d59b5d22
Samveen <samveen@yahoo.com>
2015-11-18 18:33:35 +0000

    first commit

---
+++ conf.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Configuration module for the app
+"""
+
+config={
+    'socket': '../fcgi/fcgi.sock',
+    'getGroups': {
+        'DisplayGroups': [
+            "all", "uatprovision"
+        ]
+    },
+    'decommNode': {
+        'osimage': 'centos7.1-x86_64-install-decommission'
+    }
+}
+
+
